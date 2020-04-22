a36f9b1478a55cd0fc18794e782ef282e03adbfc
yl1241 <63687198+yl1241@users.noreply.github.com>
2020-04-22 18:05:40 -0400

    Add files via upload

--- blackJack.py
+++ blackJack.py
@@ -2,8 +2,13 @@ __author__ = 'gecheng'
 
 import random as rn
 
+import random as rn
+
 class blackJackGame():
-    def __init__(self):
+    # initial the game
+    def __init__(self, player_num):
+
+        # initial two deck of cards
         self.cards = []
         rank = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K']
         suit = ['s', 'h', 'c', 'd']
@@ -12,18 +17,40 @@ class blackJackGame():
                 for s in suit:
                     self.cards.append(r+s)
 
+
         self.players = []
-        index = rn.sample(range(len(self.cards)),1)
-        self.players.append([self.cards.pop(index[0])])
-        index = rn.sample(range(len(self.cards)),1)
-        self.players[0].append(self.cards.pop(index[0]))
-
-
         self.points = []
-        self.points.append(0)
+        self.bets = []
+        self.standings = []
+        self.bust = []
+        self.split = []
+
+
+        for id in range(player_num):
+            # initial players as list, each player draw two cards
+            index = rn.sample(range(len(self.cards)),1)
+            self.players.append([self.cards.pop(index[0])])
+            index = rn.sample(range(len(self.cards)),1)
+            self.players[id].append(self.cards.pop(index[0]))
+
+            # initial points of player, set as 0
+            self.points.append(0)
+
+            # bet for each player
+            self.bets.append(1)
+
+            # whether player standing
+            self.standings.append(0)
+
+            # whether player bust
+            self.bust.append(0)
+
+            # whether player split
+            self.split.append(0)
 
 
 
+        # initial dealer hands, draw two cards
         index = rn.sample(range(len(self.cards)),1)
         self.dealer = [self.cards.pop(index[0])]
         index = rn.sample(range(len(self.cards)),1)
@@ -31,67 +58,81 @@ class blackJackGame():
 
 
 
-
+        # whether dealer bust
         self.dealerBust = 0
+        # point of dealer hand
         self.dealerPoint = 0
 
-        self.bets = []
-        self.bets.append(1)
 
-        self.standings = []
-        self.standings.append(0)
-
-        self.bust = []
-        self.bust.append(0)
-
-        self.split = 0
-
+        # whether players finish all operation
         self.finish = 0
 
+        # the final score for the player
         self.finalScore = 0
 
-
+    # get the operation for ip th player
     def playerOperation(self, ip, ope):
 
+
+        # double operation
         if ope == 'double':
+            if len(self.players[ip]) != 2:
+                raise Exception(print('double Error'))
+
             self.bets[ip] = self.bets[ip] * 2
-            self.playerOperation(self,ip, 'hit')
-            self.standings[ip] = 1
+            self.playerOperation(ip, 'hit')
             self.checkBustout()
+            if self.bust[ip] != 1:
+                self.standings[ip] = 0
+
+
+        # split operation
         if ope == 'split':
-            if len(self.players[ip]) != 2 or self.players[ip][0][0] != self.players[ip][1][0] or self.split == 1:
+            if len(self.players[ip]) != 2 or self.players[ip][0][0] != self.players[ip][1][0] or self.split[ip] == 1:
                 raise Exception(print('Split Error'))
 
-            self.players[ip] = [self.players[ip][0]]
+
             self.players.append([self.players[ip][1]])
+            self.players[ip] = [self.players[ip][0]]
 
 
             self.bets.append(self.bets[ip])
             self.standings.append(0)
             self.bust.append(0)
             self.points.append(0)
+            self.split.append(1)
 
             if self.players[ip][0][0] == 'A':
-                self.playerOperation(self, ip, 'hit')
-                self.playerOperation(self, len(self.players)-1, 'hit')
+                self.playerOperation(ip, 'hit')
+                self.playerOperation(len(self.players)-1, 'hit')
                 self.standings[ip] = 1
                 self.standings[len(self.players) -1] = 1
 
-            self.split = 1
+            self.split[ip] = 1
+
             self.checkBustout()
 
+
+        # hit operation
         if ope == 'hit':
-            index = rn.sample(range(len(self.cards)),1)
-            self.players[ip].append(self.cards.pop(index[0]))
-            self.checkBustout()
+            if self.standings[ip] == 0:
+                index = rn.sample(range(len(self.cards)),1)
+                self.players[ip].append(self.cards.pop(index[0]))
+                self.checkBustout()
+            else:
+                raise Exception(print('Hit Error'))
 
+        # standing operation
         if ope == 'std':
             self.standings[ip] = 1
 
+    # check whether the game will continue(whether all player have "standing" or "bust")
     def checkStatus(self):
         if sum(self.standings) + sum(self.bust) == len(self.players):
             self.finish = 1
 
+
+    # check whether player bust
     def checkBustout(self):
         for ip, player in enumerate(self.players):
             point = 0
@@ -105,6 +146,7 @@ class blackJackGame():
             if point > 21:
                 self.bust[ip] = 1
 
+    # dealer draw card
     def dealerDraw(self):
         if sum(self.standings) > 0:
 
@@ -148,6 +190,7 @@ class blackJackGame():
                 if point > 21:
                     self.dealerBust = 1
 
+    # calculate points of player hand
     def playerPoint(self):
         if sum(self.standings) > 0:
             for ip, player in enumerate(self.players):
@@ -162,8 +205,9 @@ class blackJackGame():
                              An += 1
                          else:
                              point += int(card[0])
-                self.points[ip] = point + min([int((21-point)/10), An]) * 10
+                     self.points[ip] = point + min([int((21-point)/10), An]) * 10
 
+    # calculate points of dealer hand
     def dealerPointCal(self):
         if self.dealerBust == 0 and sum(self.standings) > 0:
             point = 0
@@ -179,82 +223,35 @@ class blackJackGame():
             self.dealerPoint = point + min([int((21-point)/10), An]) * 10
 
 
+
+    # calculate final score of players
     def finalScoreCal(self):
-        ### To Do: Consider Blackjack
-        if self.dealerBust == 1:
-            for ip, player in enumerate(self.players):
-                if self.bust[ip] == 0:
-                    self.finalScore = self.finalScore + self.bets[ip]
-        elif self.dealerBust == 0:
-            for ip, player in enumerate(self.players):
-                if self.bust[ip] == 1:
-                    self.finalScore = self.finalScore - self.bets[ip]
-                elif self.bust[ip] == 0:
+        for ip, player in enumerate(self.players):
+            if self.bust[ip] == 1:
+                self.finalScore =self.finalScore + (-1) * self.bets[ip]
+
+            if self.bust[ip] == 0:
+                if self.dealerBust == 0:
                     if self.dealerPoint > self.points[ip]:
                         self.finalScore = self.finalScore - self.bets[ip]
                     elif self.dealerPoint < self.points[ip]:
+                        if len(self.players[ip]) == 2 and self.points[ip] == 21:
+                            self.finalScore = self.finalScore + self.bets[ip]*1.5
+                        else:
+                            self.finalScore = self.finalScore + self.bets[ip]
+
+                    elif self.dealerPoint == self.points[ip] and self.dealerPoint == 21:
+                        if len(self.players[ip]) == 2:
+                            if len(self.dealer) != 2:
+                                self.finalScore = self.finalScore + 1.5 * self.bets[ip]
+                        elif len(self.players[ip]) != 2:
+                            if len(self.dealer) == 2:
+                                self.finalScore = self.finalScore - self.bets[ip]
+
+                elif self.dealerBust == 1:
+                    if len(self.players[ip]) == 2 and self.points[ip] == 21:
+                        self.finalScore = self.finalScore + self.bets[ip]*1.5
+                    else:
                         self.finalScore = self.finalScore + self.bets[ip]
 
 
-def strategy(bl, ip):
-    point = 0
-    An = 0
-    for card in bl.players[ip]:
-        if card[0] in ['T' , 'J', 'Q', 'K']:
-            point += 10
-        elif card[0] == 'A':
-            point += 1
-            An += 1
-        else:
-            point += int(card[0])
-
-    if point + min([int((21-point)/10), An]) * 10 >= 17:
-        return 'std'
-    else:
-        return 'hit'
-
-
-
-
-rn.seed(1)
-
-finalScores = []
-
-for i in range(10000):
-    bl = blackJackGame()
-    while bl.finish == 0:
-        for ip, player in enumerate(bl.players):
-            if bl.bust[ip] == 0:
-                ope = strategy(bl, ip)
-                bl.playerOperation(ip, ope)
-
-        bl.checkStatus()
-        if bl.finish == 1:
-            bl.dealerDraw()
-            bl.dealerPointCal()
-            bl.playerPoint()
-            bl.finalScoreCal()
-
-
-    print("player hand")
-    print(bl.players)
-    print("dealer hand")
-    print(bl.dealer)
-    print("player bust")
-    print(bl.bust)
-    print("dealer bust")
-    print(bl.dealerBust)
-    print("player point")
-    print(bl.points)
-    print("dealer point")
-    print(bl.dealerPoint)
-    print("score")
-    print(bl.finalScore)
-    print("Number of cards")
-    print(len(bl.cards))
-
-    finalScores.append(bl.finalScore)
-
-
-print("total Score")
-print(sum(finalScores))
